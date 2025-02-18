a2ede3f4b59fef59c81dbea5e1bd3be36be6f6b7
RayenMettali <rayenmettali1@gmail.com>
2025-02-19 00:23:43 +0100

    firebase initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-import '/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:proxi_job/screens/welcome_screen.dart';
+import 'firebase_options.dart'; // Ensure this file exists
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
